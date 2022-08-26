57cf59de69ea2d5d0665cd56a5f0ed8092657421
BBeak <bkh3441@gmail.com>
2022-08-26 17:48:42 +0900

    회원 가입 양식 검증

--- src/main/webapp/resources/login/login.js
+++ src/main/webapp/resources/login/login.js
@@ -1,15 +1,25 @@
-const confirm ={
-    email: false,
-    password: false,
-};
+
+let flag = 0;
+
 
 $(document).ready(function(){
     addEvents()
-})
+    if (flag === 0){
+        $(".flag-0").removeClass("flag-0");
+        $(".flag-0").next().addClass("flag-1");
+        
+    }else{
+        $(".flag-1").removeClass("flag-1");
+        $(".flag-1").prev().addClass("flag-0");
+        
+    }
+});
+
+
 /**이벤트 연결 함수 */
 function addEvents(){
 /**회원가입 이메일 체크 */
-$(".normal").on("keyup",function(event){
+$(".b-button").on("onclick",function(event){
     const target = $(event.target);
     const targetName = target.attr("placeholder");
 
@@ -26,20 +36,26 @@ switch(targetName){
         /** 이메일일 경우 이쪽이 실행됨 */
 
         if(regExp.test(email)===false){
-            console.log("잘못된 이메일 양식입니다.")
-            makeWarning(target, "잘못된 이메일 양식입니다.");
-            confirm.email = false;
+            
+           alert( "잘못된 이메일 양식입니다.");
+           }
+
+        /** 비밀번호의 경우 */
+    case "Password":
+        if(password.length <= 5){
+            makeWarning(target, "잘못된 비밀번호 양식입니다.");
+            confirm.password = false;
         }else{
             removeWarning(target);
-            confirm.email = true;
+            confirm.password=true;
         }
-}
+    }
 
 
 
 })
 /** 화면 전환 */
-let flag = 0;
+
 
 $('.signin').on("click", function(){
     if(flag === 0){
@@ -48,10 +64,8 @@ $('.signin').on("click", function(){
 
     $(".form").addClass("movingForm");
     $(".form").removeClass("srartForm");
-    
     $('.hello').show();
-    $('.welcome').hide();
-
+    $('.welcome').hide(); 
     $(".move").css("background-position", "right");
     
     setTimeout(function(){
@@ -71,14 +85,12 @@ $('.signin').on("click", function(){
     flag = 1;
     }else{
         $(".move").removeClass("moving");
-    $(".move").addClass("start");
-
+    $(".move").addClass("start");    
     $(".form").removeClass("movingForm");
     $(".form").addClass("srartForm");
-    
-    $('.hello').hide();
-    $('.welcome').show();
 
+    $('.hello').hide();
+    $('.welcome').show();    
     $(".move").css("background-position", "left");
     
     setTimeout(function(){
